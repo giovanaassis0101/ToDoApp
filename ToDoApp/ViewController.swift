bc0b168de7aa6df2e4767c2af4d095f8e0f9a52d
giovanaassis0101 <giovanaassisstos@gmail.com>
2025-06-08 13:40:31 -0300

    refactor and organize code

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -8,83 +8,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let labelContainer = UIView()
+    let label = UILabel()
+    let button = UIButton(type: .system)
+    
+    let originalTextColor: UIColor =  .black
+    let originalContainerColor: UIColor = .systemPink
+    let originalViewColor: UIColor = .white
+    let originalText = "Hello World!"
+    
+    var isOriginalState  = true
 
     //override - significa substituir a função que já existe. viewDidLoad - é uma função que já existe na UIViewController
     override func viewDidLoad() {
         //está chamando a função que já existe
         super.viewDidLoad()
         
-        let originalTextColor =  UIColor.black
-        let originalContainerColor = UIColor.systemPink
-        let originalViewColor = UIColor.white
-        let originalText = "Hello World!"
-        
-        let labelContainer = UIView()
-        labelContainer.backgroundColor = originalContainerColor
-        
-        
-        //criação e configuração da label
-        let label = UILabel()
-        label.text = originalText
-        label.textColor = originalTextColor
-        
-        //configuração da view
-        view.backgroundColor = originalViewColor
-        
-        //a view já existe no UiViewController, então ela sempre vai estar aqui se for uma UIViewController.
-        //adicionando a label na hierarquia da view. (label é filha da view e a view é pai da label).
+        setupView()
+        setupOriginalState()
+        setupContraints()
+    }
+    
+    func setupView() {
         view.addSubview(labelContainer)
         labelContainer.addSubview(label)
-        
-        //configuraçòes obrigatória para criar contraints programaticamente.
-        labelContainer.translatesAutoresizingMaskIntoConstraints = false
-        
-        //criação das constraints da label
-        labelContainer.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        labelContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
-        label.translatesAutoresizingMaskIntoConstraints =  false
-        
-        label.topAnchor.constraint(equalTo: labelContainer.topAnchor, constant: 20).isActive = true
-        label.bottomAnchor.constraint(equalTo: labelContainer.bottomAnchor, constant: -20).isActive = true
-        label.leadingAnchor.constraint(equalTo: labelContainer.leadingAnchor, constant: 20).isActive = true
-        label.trailingAnchor.constraint(equalTo: labelContainer.trailingAnchor, constant: -20).isActive = true
+        view.addSubview(button)
         
         labelContainer.layer.cornerRadius = 20
-        labelContainer.layer.shadowOpacity = 5
-        
-        var isOriginalState = true
-        
-        let action = UIAction(title: "") { _ in
-            UIView.animate(withDuration: 2, animations: {
-                if isOriginalState == true {
-                    isOriginalState = false
-                    label.text = "Primeiro Button"
-                    label.textColor = UIColor.white
-                    labelContainer.backgroundColor = UIColor.systemPink
-                    self.view.backgroundColor = UIColor.black
-                } else {
-                    isOriginalState = true
-                    label.text = originalText
-                    label.textColor = originalTextColor
-                    labelContainer.backgroundColor = originalContainerColor
-                    self.view.backgroundColor = originalViewColor
-                }
-            })
-        }
-        let button = UIButton(type: .system, primaryAction: action)
-        button.setTitle("Button", for: .normal)
-        
-        view.addSubview(button)
-        button.translatesAutoresizingMaskIntoConstraints =  false
-        button.centerXAnchor.constraint(equalTo: labelContainer.centerXAnchor).isActive = true
-        
-        button.topAnchor.constraint(equalTo: labelContainer.bottomAnchor, constant: 30).isActive = true
-        
-        
-        
+        button.setTitle("Mudar estado", for: .normal)
+        button.addAction(UIAction { _ in
+            if self.isOriginalState == true {
+                self.setupSecondaryState()
+            } else {
+                self.setupOriginalState()
+            }
+        }, for: .touchUpInside)
     }
-
+    
+    func setupOriginalState() {
+        isOriginalState = true
+        
+        label.text = originalText
+        label.textColor = originalTextColor
+        labelContainer.backgroundColor = originalContainerColor
+        view.backgroundColor = originalViewColor
+    }
+    
+    func setupSecondaryState() {
+        isOriginalState = false
+        
+        label.text = "Primeiro Button"
+        label.textColor = UIColor.white
+        labelContainer.backgroundColor = UIColor.systemPink
+        view.backgroundColor = UIColor.black
+    }
+    
+    func setupContraints() {
+        labelContainer.translatesAutoresizingMaskIntoConstraints = false
+        label.translatesAutoresizingMaskIntoConstraints =  false
+        button.translatesAutoresizingMaskIntoConstraints =  false
+        
+        NSLayoutConstraint.activate([
+            labelContainer.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            labelContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.topAnchor.constraint(equalTo: labelContainer.topAnchor, constant: 20),
+            label.bottomAnchor.constraint(equalTo: labelContainer.bottomAnchor, constant: -20),
+            label.leadingAnchor.constraint(equalTo: labelContainer.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: labelContainer.trailingAnchor, constant: -20),
+            button.centerXAnchor.constraint(equalTo: labelContainer.centerXAnchor),
+            button.topAnchor.constraint(equalTo: labelContainer.bottomAnchor, constant: 30)
+        ])
+    }
 
 }
 
